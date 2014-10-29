7c0c74c08b045c9422f286d838c304f74c8f3273
angeloocana <angeloocana@gmail.com>
2014-10-29 13:56:54 -0200

    Fix: Where Jumper Can Go

--- JumpOverJump/JumpOverJump/Scripts/Game/Game.js
+++ JumpOverJump/JumpOverJump/Scripts/Game/Game.js
@@ -1,13 +1,13 @@
 JumpOverJump.Game = function(game) {
     this.background = null;
-    this.jumpers = null;
+    this.jumpers = [];
     this.squares = [[]];
     this.squareWidth = 78;
     this.squareHeight = 78;
     this.boardX = 20;
     this.boardY = 20;
     this.ready = false;
-    this.selectedJumper = null;
+    this.selectedJumperIndex = null;
 };
 
 JumpOverJump.Game.prototype = {	
@@ -20,8 +20,15 @@ JumpOverJump.Game.prototype = {
         this.buildJumpers();
 	},
     
-    getSquareKey : function(x,y){
-        return 'square' + x + '_' + y;
+    buildSquares: function(){        
+        var isblack = false;
+        for(var x = 0; x < 8; x++){            
+            for(var y = 0; y < 8; y++){
+                this.createSquare(x, y, isblack);
+                isblack = !isblack;
+            }	
+            isblack = !isblack;
+        }	
     },
     
     createSquare: function (x, y, isblack) {
@@ -36,35 +43,22 @@ JumpOverJump.Game.prototype = {
         square.defaultAnimation = isblack ? "black" : "white";
         square.animations.play(square.defaultAnimation, 1, false);                
         
-        this.addSquare(x,y,square,null);            
-    },
-
-    squareClicked: function(object, pointer) {
-        console.log(object.x);
-        console.log(object.y);
-        console.log(object);
-        this.add.tween(this.selectedJumper).to({x:object.x, y:object.y}, 1000, Phaser.Easing.Quadratic.InOut, true);
-        this.resetSquaresAnimation();
-        this.squares[this.selectedJumper.square.x][this.selectedJumper.square.y].jumper = null;
-        this.squares[x][y].jumper = this.selectedJumper; 
-        this.squares[x][y].jumper.x = x;
-        this.squares[x][y].jumper.y = y;
-        this.selectedJumper = null;
+        this.addSquare(x,y,square);            
     },
     
-    buildSquares: function(){        
-        var isblack = false;
-        for(var x = 0; x < 8; x++){            
-            for(var y = 0; y < 8; y++){
-                this.createSquare(x, y, isblack);
-                isblack = !isblack;
-            }	
-            isblack = !isblack;
-        }	
+    addSquare: function(x, y, square){
+        if(this.squares == undefined)
+            this.squares = new Array();
+        if(this.squares[x] == undefined)
+            this.squares[x] = new Array();
+        
+        square.board = {x:x,y:y};
+        this.squares[x][y] = square;
     },
     
     buildJumpers: function(){        
         var isblack = false;
+        this.jumpers = [];
         for(var y = 0; y < 8; y = y + 7){            
             for(var x = 0; x < 8; x++){                        
                 this.createJumper(x, y, isblack);
@@ -84,22 +78,11 @@ JumpOverJump.Game.prototype = {
         jumper.animations.play(jumperAnimation, 1, false);
         jumper.events.onInputDown.add(this.jumperClicked, this);
         jumper.events.onInputOver.add(this.jumperOver, this);
-        jumper.square = {x:x,y:y};
-        this.addSquare(x,y,null,jumper);            
+        jumper.board = {x:x,y:y};
+        jumper.i = this.jumpers.length;
+        this.jumpers[jumper.i] = jumper;
     },
-    
-    addSquare: function(x, y, square, jumper){
-        if(this.squares == undefined)
-            this.squares = new Array();
-        if(this.squares[x] == undefined)
-            this.squares[x] = new Array();
         
-        this.squares[x][y] = {
-            square : square == null ? this.squares[x][y].square : square, 
-            jumper : jumper
-        };
-    },
-    
     jumperOver: function (object, pointer) {
         this.whereJumperCanGo(object);
     },
@@ -109,11 +92,11 @@ JumpOverJump.Game.prototype = {
     },
     
     whereJumperCanGo: function(jumper){
-        var x = jumper.square.x;
-        var y = jumper.square.y;
-        this.selectedJumper = jumper;
+        var x = jumper.board.x;
+        var y = jumper.board.y;
+        this.selectedJumperIndex = jumper.i;
         this.resetSquaresAnimation();
-        
+                
         if(this.squares[x-1])        
         {            
             this.jumperCanGo(x-1,y-1);   
@@ -135,24 +118,42 @@ JumpOverJump.Game.prototype = {
     resetSquaresAnimation : function(){
         for(var x = 0; x < 8; x++){     
             for(var y = 0; y < 8; y++){                        
-                var square = this.squares[x][y].square;
+                var square = this.squares[x][y];
                 square.animations.play(square.defaultAnimation, 1, false);
                 square.events.onInputDown.remove(this.squareClicked, this);
             }	
         }	
     },
     
-    jumperCanGo: function(x,y){
+    jumperCanGo: function(x,y){                
         var column = this.squares[x];
         if(column[y])
-            {
-                var jumper = column[y].jumper;
-                var square = column[y].square;
-                if(jumper == null || jumper == undefined){
-                    square.animations.play("canGo", 1, false);
-                    square.events.onInputDown.add(this.squareClicked, this);
-                }
-            } 
+        {                
+            //console.log('for  x:' + this.selectedJumper.square.x + ' y:'+ this.selectedJumper.square.y);
+            for(var i = 0; i < this.jumpers.length; i++){                
+                console.log('jx: ' + this.jumpers[i].x +' x: '+  x +' jy: '+ this.jumpers[i].y + ' y:' + y);
+                if(this.jumpers[i].board.x == x && this.jumpers[i].board.y == y)
+                    return false;
+            }
+            
+            var square = column[y];
+            square.animations.play("canGo", 1, false);
+            square.events.onInputDown.add(this.squareClicked, this);
+        } 
+    },
+    
+    squareClicked: function(square, pointer) {
+        var selectedJumper = this.getSelectedJumper();
+        this.add.tween(selectedJumper).to({x:square.x, y:square.y}, 1000, Phaser.Easing.Quadratic.InOut, true);
+        
+        selectedJumper.board.x = square.board.x;
+        selectedJumper.board.y = square.board.y;
+        this.jumpers[selectedJumper.i] = selectedJumper;
+        this.whereJumperCanGo(selectedJumper);
+    },
+    
+    getSelectedJumper: function(){
+        return this.jumpers[this.selectedJumperIndex];
     },
 
 	update: function () {
