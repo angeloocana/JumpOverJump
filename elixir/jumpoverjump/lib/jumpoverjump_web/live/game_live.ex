8ae1b656f17f1f6f3fd86122847fd679ee5187b8
Angelo Ocana Martins <angeloocana@gmail.com>
2024-09-21 20:49:20 -0400

    Playing with Phoenix forms

--- elixir/jumpoverjump/lib/jumpoverjump_web/live/game_live.ex
+++ elixir/jumpoverjump/lib/jumpoverjump_web/live/game_live.ex
@@ -2,7 +2,7 @@ defmodule JumpoverjumpWeb.GameLive do
   use JumpoverjumpWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, turn: :white)}
+    {:ok, assign(socket, turn: :white, last_move: "", form: to_form(%{from: "", to: ""})) }
   end
 
   def render(assigns) do
@@ -11,13 +11,18 @@ defmodule JumpoverjumpWeb.GameLive do
       <h1>Jumpoverjump</h1>
 
       <%= @turn %>
-      <.button phx-click="move">Move</.button>
+      <%= @last_move %>
+      <.simple_form for={@form} phx-submit="move">
+        <.input field={@form[:from]} value={@form.params.from} />
+        <.input field={@form[:to]} value={@form.params.to} />
+        <.button>Move</.button>
+      </.simple_form>
     </div>
     """
   end
 
-  def handle_event("move", _params, socket) do
+  def handle_event("move", %{"from" => from, "to" => to}, socket) do
     new_turn = if socket.assigns.turn == :white, do: :black, else: :white
-    {:noreply, assign(socket, turn: new_turn)}
+    {:noreply, assign(socket, turn: new_turn, last_move: from <> " " <> to)}
   end
 end
