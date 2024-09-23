ccae772887ff0131443147729873797b00dc0df5
Angelo Ocana Martins <angeloocana@gmail.com>
2024-09-22 21:08:56 -0400

    Initialize board

--- elixir/jumpoverjump/lib/jumpoverjump_web/live/game_live.ex
+++ elixir/jumpoverjump/lib/jumpoverjump_web/live/game_live.ex
@@ -1,28 +1,31 @@
 defmodule JumpoverjumpWeb.GameLive do
   use JumpoverjumpWeb, :live_view
 
+  @board_last_index 7
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, turn: :white, last_move: "", form: to_form(%{from: "", to: ""})) }
-  end
-
-  def render(assigns) do
-    ~H"""
-    <div>
-      <h1>Jumpoverjump</h1>
-
-      <%= @turn %>
-      <%= @last_move %>
-      <.simple_form for={@form} phx-submit="move">
-        <.input field={@form[:from]} value={@form.params.from} />
-        <.input field={@form[:to]} value={@form.params.to} />
-        <.button>Move</.button>
-      </.simple_form>
-    </div>
-    """
+    {:ok, assign(socket,
+      turn: :white,
+      last_move: "",
+      form: to_form(%{from: "", to: ""}),
+      board: initialize_board()
+    )}
   end
 
   def handle_event("move", %{"from" => from, "to" => to}, socket) do
     new_turn = if socket.assigns.turn == :white, do: :black, else: :white
     {:noreply, assign(socket, turn: new_turn, last_move: from <> " " <> to)}
+  end
+
+  defp initialize_board do
+    for i <- 0..@board_last_index do
+      for _j <- 0..@board_last_index do
+        cond do
+          i == 0 -> 1
+          i == @board_last_index -> 2
+          true -> 0
+        end
+      end
+    end
   end
 end
